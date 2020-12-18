38ea84d645a6475fabb12454042980de174aec71
Travis Prosser <travisprosser@gmail.com>
2020-12-18 17:02:14 -0400

    added template

---
+++ template.py
@@ -0,0 +1,16 @@
+
+def part_a():
+    pass
+
+def part_b():
+    pass
+
+if __name__ == "__main__":
+    with open('input.txt') as f:
+        raw_data = f.read()
+    
+    a = part_a()
+    print(f"Answer A: {a}")
+    
+    b = part_b()
+    print(f"Answer B: {b}")
