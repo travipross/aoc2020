a8cce90338332c4bf979eb9fddc1aecae235c9a9
Travis Prosser <travisprosser@gmail.com>
2020-12-20 11:41:59 -0400

    update template

--- template.py
+++ template.py
@@ -1,16 +1,26 @@
 
-def part_a():
+def read_data(filename):
+    with open('input.txt') as f:
+        raw_data = f.read()
+    return raw_data
+
+def parse_data(raw_data):
+    parsed_data = raw_data 
+
+    return parsed_data
+
+def part_a(parsed_data):
     pass
 
-def part_b():
+def part_b(parsed_data):
     pass
 
 if __name__ == "__main__":
-    with open('input.txt') as f:
-        raw_data = f.read()
-    
-    a = part_a()
+    raw_data = read_data('input.txt')
+    parsed_data = parse_data(raw_data)
+
+    a = part_a(parsed_data)
     print(f"Answer A: {a}")
     
-    b = part_b()
+    b = part_b(parsed_data)
     print(f"Answer B: {b}")
